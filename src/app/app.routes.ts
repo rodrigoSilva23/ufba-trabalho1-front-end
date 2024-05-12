7300a140fbb8565649ae9afe7adb4a064baf489c
rodrigoDev <rodriguinho2456@gmail.com>
2024-05-12 09:27:47 -0300

    [refactor] modify input for input Box

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,20 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { authGuard } from './guards/auth.guard';
 import { NotFoundComponent } from './components/pages/not-found/not-found.component';
 import { AuthComponent } from './components/pages/auth/auth.component';
-import { LayoutComponent } from './components/pages/layout/layout.component';
+import { LayoutComponent } from './components/template/layout/layout.component';
 import { AddressComponent } from './components/pages/address/address.component';
+import { SignupComponent } from './components/pages/signup/signup.component';
 
 export const routes: Routes = [
   {
     path: 'auth',
    component: AuthComponent,title: 'login',
+
+  },
+
+  {
+    path: 'signup',
+   component: SignupComponent,title: 'signup',
 
   },
 
