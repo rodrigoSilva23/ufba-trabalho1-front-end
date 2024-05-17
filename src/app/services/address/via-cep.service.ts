7d21a14c5d3feaf58d0081a9de0651788c8fba40
rodrigoDev <rodriguinho2456@gmail.com>
2024-05-16 21:25:52 -0300

    [feat] created  service via cep

---
+++ src/app/services/address/via-cep.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+
+interface CepResponse {
+  bairro: string;
+  cep: number;
+  complemento: string;
+  ddd: number;
+  gia: string;
+  ibge: number;
+  localidade: string;
+  logradouro: string;
+  siafi: number;
+  uf: string;
+}
+@Injectable({
+  providedIn: 'root',
+})
+export class ViaCepService {
+  baseUrl = 'https://viacep.com.br/ws/';
+  constructor(private httpClient: HttpClient) {}
+  getCep(cep: string) {
+    return this.httpClient.get<CepResponse>(`${this.baseUrl}${cep}/json/`);
+  }
+}
